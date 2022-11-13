b010915fd56ccfeb6b29c7678f6dbf969e79de6c
Stas <smaster950@gmail.com>
2022-11-13 18:49:22 +0300

    Complete Exercise66

--- Exercise66/Program.cs
+++ Exercise66/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите число M: ");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите число N: ");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+
+if (num1 < num2)
+{
+    Sum(num1, num2);
+}
+else
+{
+    Console.WriteLine($"Вывод невозможен, {num1} больше {num2}");
+}
+
+void Sum(int num1, int num2)
+{
+    Console.Write(CalculateSum(num1 - 1, num2));
+}
+
+int CalculateSum(int num1, int num2)
+{
+    int res = num1;
+    if (num1 == num2)
+        return 0;
+    else
+    {
+        num1++;
+        res = num1 + CalculateSum(num1, num2);
+        return res;
+    }
+}
