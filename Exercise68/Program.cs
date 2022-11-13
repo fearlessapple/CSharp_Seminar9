e160f382ff1e203f1bc5ef1caa41cabf2c619919
Stas <smaster950@gmail.com>
2022-11-13 19:17:29 +0300

    Complete Exercise68

--- Exercise68/Program.cs
+++ Exercise68/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите число M: ");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите число N: ");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+
+Function(num1, num2);
+
+
+void Function(int num1, int num2)
+{
+    Console.Write(CalculateFunction(num1, num2));
+}
+
+int CalculateFunction(int num1, int num2)
+{
+    if (num1 == 0)
+    {
+        return num2 + 1;
+    }
+    else if (num1 >0 &&  num2 == 0)
+    {
+        return CalculateFunction(num1 - 1, 1);
+    }
+    else
+    {
+        return (CalculateFunction(num1 - 1, CalculateFunction(num1, num2 - 1)));
+    }
+}
